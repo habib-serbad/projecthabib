70cecdcfb6ef2934acf173c201749c9a349d2bd1
habib-serbad <muhamadhabibullah98@gmail.com>
2021-12-21 18:49:42 +0700

    Initial commit

---
+++ polls/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse
+def index(request):
+    informasi="""
+    <!DOCTYPE html>
+    <html>
+        <head></head>
+    
+
+        <body>
+        <h1>PERCOBAAN OM</h1>
+        </body>
+    
+    
+    
+    
+    
+    
+    
+    
+    </html>
+
+
+    """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return HttpResponse(informasi)
+
